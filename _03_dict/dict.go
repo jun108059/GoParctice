4bdf65497c2a27dace842bf9bdbc5db0da8e89e5
YoungJun Park <youngjun108059@gmail.com>
2022-03-20 23:49:43 +0900

    [Feature] Dictionary Add/Update Method

--- _03_dict/dict.go
+++ _03_dict/dict.go
@@ -6,7 +6,9 @@ import "errors"
 type Dictionary map[string]string
 
 var (
-	errNotFound = errors.New("not Found")
+	errNotFound     = errors.New("not Found")
+	errCantUpdate   = errors.New("can not Update non-existing word")
+	errAlreadyExist = errors.New("already Exist")
 )
 
 // Search 검색 key의 value 찾기
@@ -17,4 +19,33 @@ func (d Dictionary) Search(word string) (string, error) {
 		return value, nil
 	}
 	return "", errNotFound
+}
+
+// Add map dictionary에 데이터 추가하기
+func (d Dictionary) Add(key string, value string) error {
+	_, err := d.Search(key)
+	if err == errNotFound {
+		// map 데이터 할당
+		d[key] = value
+	} else {
+		return errAlreadyExist
+	}
+	return nil
+}
+
+// Update map dictionary 데이터 업데이트
+func (d Dictionary) Update(key string, value string) error {
+	_, err := d.Search(key)
+	switch err {
+	case nil:
+		d[key] = value
+	case errNotFound:
+		return errCantUpdate
+	}
+	return nil
+}
+
+// Delete map dictionary 데이터 삭제
+func (d Dictionary) Delete(key string) {
+	delete(d, key)
 }
