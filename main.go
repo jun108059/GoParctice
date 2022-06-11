8dc6a2d5bd6aeafa63a061135d4431fa2ea958c2
YoungJun Park <youngjun108059@gmail.com>
2022-06-11 23:19:09 +0900

    [Feature] GET Method - Json Response

--- main.go
+++ main.go
@@ -2,14 +2,22 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 var router *gin.Engine
 
 func main() {
-
 	// 기본 라우터 생성
 	router = gin.Default()
+
+	// HttpStatus : Data
+	router.GET("/abc", func(c *gin.Context) {
+		// c.String(http.StatusOK, "Hello World!")
+		c.JSON(http.StatusOK, gin.H{
+			"responseData": "Hello",
+		})
+	})
 
 	// 모든 템플릿을 미리 로드 > 다시 로드할 일 없음 > HTML 서빙이 엄청 빠르다!
 	router.LoadHTMLGlob("templates/*")
@@ -18,6 +26,6 @@ func main() {
 	initializeRoutes()
 
 	// 애플리케이션 시작!
-	router.Run()
+	router.Run("localhost:8080")
 
 }
